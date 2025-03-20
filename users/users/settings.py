00b1a57c2ec533596fd3bccc946311891339ff20
Davit <davit.kv8@gmail.com>
2025-03-20 12:22:38 +0400

    FIX Procfile! load env vars correctly

--- users/users/settings.py
+++ users/users/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 
 from datetime import timedelta
 
-load_dotenv("/opt/elasticbeanstalk/deployment/env")
+# load_dotenv("/opt/elasticbeanstalk/deployment/env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
